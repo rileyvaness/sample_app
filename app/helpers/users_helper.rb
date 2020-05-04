5293a0286efacdacf90363ca4d6870fdbc9c2f3d
Riley Van Ess <rileyvaness@gmail.com>
2020-05-04 15:38:30 +0000

    finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+   # Returns the Gravatar for the given user.
+    def gravatar_for(user, size: 180)
+    gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/471c6acd54d2b08d1576d153b40102c4?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
