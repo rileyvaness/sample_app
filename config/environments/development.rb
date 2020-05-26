adde5289a6a389bb9a65d2505cebec09e1bde7d2
Riley Van Ess <rileyvaness@gmail.com>
2020-05-26 17:27:43 +0000

    Add account Activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,8 @@ Rails.application.configure do
   
     # Allow connections to local server.
   config.hosts.clear
+
+config.action_mailer.raise_delivery_errors = false
+host = '<hex string>.vfs.cloud9.us-east-2.amazonaws.com'     # Cloud IDE
+config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 end
